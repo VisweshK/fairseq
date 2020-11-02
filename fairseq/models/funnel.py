4a5d0b01b9e5087d6cc74ff9af182ee054713ec7
Viswesh Krishna <krishnaviswesh@gmail.com>
2020-11-02 09:54:37 +0530

    fix: smaller arch

--- fairseq/models/funnel.py
+++ fairseq/models/funnel.py
@@ -269,14 +269,14 @@ def base_architecture(args):
 
 @ register_model_architecture("funnel_transformer", "funnel_transformer_iwslt_de_en")
 def funnel_transformer_iwslt_de_en(args):
+    base_architecture(args)
     args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
     args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 1024)
     args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 4)
-    args.encoder_layers = getattr(args, "encoder_layers", 6)
-    args.num_blocks = getattr(args, "num_blocks", 6)
+    args.encoder_layers = getattr(args, "encoder_layers", 1)
+    args.num_blocks = getattr(args, "num_blocks", 1)
     args.upsample = getattr(args, "upsample", True)
     args.decoder_embed_dim = getattr(args, "decoder_embed_dim", 512)
     args.decoder_ffn_embed_dim = getattr(args, "decoder_ffn_embed_dim", 1024)
     args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 4)
-    args.decoder_layers = getattr(args, "decoder_layers", 6)
-    base_architecture(args)
+    args.decoder_layers = getattr(args, "decoder_layers", 1)
