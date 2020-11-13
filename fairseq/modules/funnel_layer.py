99351879fd275d7db77af829b07a9d854749a10a
Viswesh Krishna <krishnaviswesh@gmail.com>
2020-11-13 15:36:27 +0530

    fix: torch type errors

--- fairseq/modules/funnel_layer.py
+++ fairseq/modules/funnel_layer.py
@@ -142,8 +142,8 @@ class FunnelEncoderLayer(nn.Module):
     
     def time_compress_encoder_padding_mask(self, mask):
         """Max pool along time axis"""
-        mask = torch.unsqueeze(mask, 0)
-        return self.compress_encoder_padding_mask_fn(mask)
+        mask = torch.unsqueeze(mask, 0).to(dtype=torch.int32)
+        return self.compress_encoder_padding_mask_fn(mask) > 0
 
     def forward(self, x, encoder_padding_mask, attn_mask: Optional[Tensor] = None):
         """Either adapted self-attention, or normal self-attention"""
