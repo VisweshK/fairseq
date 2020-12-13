1e04cc753628c1843b37012d5cd4af0f18430837
Viswesh Krishna <krishnaviswesh@gmail.com>
2020-12-13 17:29:19 +0530

    fix: var naming error

--- fairseq/modules/funnel_layer.py
+++ fairseq/modules/funnel_layer.py
@@ -23,7 +23,7 @@ class FunnelEncoderLayer(nn.Module):
         # Funnel Args
         self.stride = stride
         self.embed_dim = embed_dim
-        self.ffn_embed_dim = self.embed_dim * args.ffn_embed_factor
+        self.ffn_embed_dim = self.embed_dim * args.encoder_ffn_embed_factor
         self.block_id = block_id
         self.block_num = block_num
         self.should_compress_query = should_compress_query
