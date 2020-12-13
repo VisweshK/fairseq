3f38347a2d1be960e58e280c4cea695f0babb043
Viswesh Krishna <krishnaviswesh@gmail.com>
2020-12-13 18:57:07 +0530

    fix: use stride for encoder mask time compression

--- fairseq/models/funnel.py
+++ fairseq/models/funnel.py
@@ -220,7 +220,8 @@ class FunnelEncoder(TransformerEncoder):
         for layer in self.layers:
             x = layer(x, encoder_padding_mask,
                       encoder_padding_mask.size(1) <= 4)
-            if layer.block_id == self.encoder_layers - 1 and self.should_time_compress and encoder_padding_mask.size(1) > 4:
+            if layer.block_id == self.encoder_layers - 1 and self.should_time_compress \
+                    and encoder_padding_mask.size(1) >= self.stride * 4:
                 encoder_padding_mask = self.time_compress_encoder_padding_mask(
                     encoder_padding_mask)
             if layer.block_id == self.encoder_layers - 1 and layer.block_num == 0:
